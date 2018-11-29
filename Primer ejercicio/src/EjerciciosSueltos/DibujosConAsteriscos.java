ae941f4b5f80bbf386d541bf76e589a91dd090d8
Cristinamf <krismf98@gmail.com>
2018-11-29 12:52:40 +0100

    Segundo commit

--- Primer ejercicio/src/EjerciciosSueltos/DibujosConAsteriscos.java
+++ Primer ejercicio/src/EjerciciosSueltos/DibujosConAsteriscos.java
@@ -32,48 +32,63 @@ public class DibujosConAsteriscos {
 		}
 	}
 	
-	static void tablero(int f, int c) {
-		int s=0;
-		int j=0;
-		int m=0;
-		
-		while (m<4) {
-			while (j<3) {
-				s=0;
-				while (s<4) {
-					for(int i=1; i<=3; i++) {
+//	static void tableromio(int f, int c) {
+//		int s=0;
+//		int j=0;
+//		int m=0;
+//		
+//		while (m<4) {
+//			while (j<3) {
+//				s=0;
+//				while (s<4) {
+//					for(int i=1; i<=3; i++) {
+//						System.out.print("*");
+//					}
+//					for (int i=3; i>=1; i--) {
+//						System.out.print(" ");
+//					}
+//					s++;
+//				}
+//				System.out.println("");
+//				j++;
+//			}
+//			while(j>0) {
+//				s=3;
+//				while(s>0) {
+//					for(int i=1; i<=3; i++) {
+//						System.out.print(" ");
+//					}
+//					for (int i=3; i>=1; i--) {
+//						System.out.print("*");
+//					}
+//					s--;
+//				}
+//				System.out.println("");
+//				j--;
+//			}
+//			m++;
+//		}
+//	}
+		static void tablero(int f, int c, int l) {
+			int ctxt = c * l;
+			int ftxt = f * l;
+			for (int i = 0; i < ftxt; i++) {
+				for (int j = 0; j < ctxt; j++) {
+					int ctab = j / l;
+					int ftab = i / l;
+					if ((ftab % 2 == 0 && ctab % 2 != 0) || (ftab % 2 != 0 && ctab % 2 == 0))
 						System.out.print("*");
-					}
-					for (int i=3; i>=1; i--) {
+					else
 						System.out.print(" ");
-					}
-					s++;
 				}
-				System.out.println("");
-				j++;
+				System.out.println();
 			}
-			while(j>0) {
-				s=3;
-				while(s>0) {
-					for(int i=1; i<=3; i++) {
-						System.out.print(" ");
-					}
-					for (int i=3; i>=1; i--) {
-						System.out.print("*");
-					}
-					s--;
-				}
-				System.out.println("");
-				j--;
-			}
-			m++;
 		}
-	}
 		
 	public static void main (String args []) {
 //		volcan (6);
 //		mosaico(8, 8);
-		tablero (8, 8);
+//		tableromio (8, 8);
 		
 	}
 }
